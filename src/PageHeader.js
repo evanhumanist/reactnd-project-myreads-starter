0df6144ab291fb591f35458b1ad4c5a7c346296c
evanh <evanhumanist@gmail.com>
2018-11-04 01:19:08 -0500

    feature: moved components out of the App.js file

---
+++ src/PageHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PageHeader() {
+    return(
+        <div className="list-books-title">
+            <h1>MyReads</h1>
+        </div>
+    )
+}
+
+export default PageHeader
